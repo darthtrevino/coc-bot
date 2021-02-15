b02bfe39bc990050e1bfdc48c652bdc13b39832a
Chris Trevino <chtrevin@microsoft.com>
2021-02-15 13:09:52 -0800

    correct .env file generation

--- packages/bot/writeDeployEnvFile.js
+++ packages/bot/writeDeployEnvFile.js
@@ -2,5 +2,8 @@
 const fs = require('fs')
 const path = require('path')
 
+if (process.env.BOT_TOKEN == null) {
+	throw new Error(`process.env.BOT_TOKEN must be defined`)
+}
 const content = `BOT_TOKEN=${process.env.BOT_TOKEN}`
 fs.writeFileSync(path.join(__dirname, 'dist/.env'), content)
