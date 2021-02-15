a691f4ab4682e302ab82ad2365de3c0c5dda57b1
Chris Trevino <chtrevin@microsoft.com>
2021-02-15 13:19:42 -0800

    add newline to envfile

--- packages/bot/writeDeployEnvFile.js
+++ packages/bot/writeDeployEnvFile.js
@@ -5,5 +5,8 @@ const path = require('path')
 if (process.env.BOT_TOKEN == null) {
 	throw new Error(`process.env.BOT_TOKEN must be defined`)
 }
-const content = `BOT_TOKEN=${process.env.BOT_TOKEN}`
+
+const content = `BOT_TOKEN=${process.env.BOT_TOKEN}
+`
+
 fs.writeFileSync(path.join(__dirname, 'dist/.env'), content)
